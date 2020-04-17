0c17abef2d556be1f87c72db4ee5255019608533
Sebastian Nemeth <sebastian@monkmage.com>
2020-04-17 18:08:48 +0300

    fix font flash on load

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
 const path = require("path");
 const withFonts = require("next-fonts");
 
-module.exports = withFonts({
+const fontsConfig = withFonts({
   target: "serverless",
   enableSvg: true,
   webpack(config) {
     config.resolve.modules.push(path.resolve("./"));
+
+    // Override the default limit for next-fonts and ensure that all fonts are loaded as Bas64.
+    const fontRule = config.module.rules[config.module.rules.length - 1];
+    fontRule.use[0].options.limit = 999999999999;
+    console.log(JSON.stringify(fontRule, null, 2));
     return config;
   }
 });
+
+module.exports = fontsConfig;
--- styles/fonts.ts
+++ styles/fonts.ts
@@ -1,44 +1,21 @@
 import { css } from "styled-components";
-const base = `/fonts`;
-
-const LetterGothicEot = `${base}/LetterGothic/LetterGothicStdSlanted.eot`;
-const LetterGothicSvg = `${base}/LetterGothic/LetterGothicStdSlanted.svg`;
-const LetterGothicTtf = `${base}/LetterGothic/LetterGothicStdSlanted.ttf`;
-const LetterGothicWoff = `${base}/LetterGothic/LetterGothicStdSlanted.woff`;
-const EditionEot = `${base}/Edition/edition_-webfont.eot`;
-const EditionSvg = `${base}/Edition/edition_-webfont.svg`;
-const EditionTtf = `${base}/Edition/edition_-webfont.ttf`;
-const EditionWoff = `${base}/Edition/edition_-webfont.woff`;
-const LucidaConsoleEot = `${base}/LucidaConsole/LucidaConsole.eot`;
-const LucidaConsoleSvg = `${base}/LucidaConsole/LucidaConsole.svg`;
-const LucidaConsoleTtf = `${base}/LucidaConsole/LucidaConsole.ttf`;
-const LucidaConsoleWoff = `${base}/LucidaConsole/LucidaConsole.woff`;
+import LetterGothicStdSlantedWoff from '../public/fonts/LetterGothic/LetterGothicStdSlanted.woff';
+import EditionRegularWoff from '../public/fonts/Edition/edition_-webfont.woff';
+import LucidaConsoleWoff from '../public/fonts/LucidaConsole/LucidaConsole.woff';
 
 export const fonts = css`
     @font-face {
     font-family: 'LetterGothicSlanted';
-	src: url('${LetterGothicEot}');
-    src: url('${LetterGothicEot}#iefix') format('embedded-opentype'),
-         url('${LetterGothicWoff}') format('woff'),
-         url('${LetterGothicTtf}') format('truetype'),
-         url('${LetterGothicSvg}#LetterGothicSlanted') format('svg');     
+	src: url('${LetterGothicStdSlantedWoff}');    
 }
 
 @font-face {
     font-family: 'EditionRegular';
-    src: url('${EditionEot}');
-    src: url('${EditionEot}#iefix') format('embedded-opentype'),
-         url('${EditionWoff}') format('woff'),
-         url('${EditionTtf}') format('truetype'),
-         url('${EditionSvg}#EditionRegular') format('svg');    
+    src: url('${EditionRegularWoff}');
 }
 
 @font-face {
     font-family: 'LucidaConsole';
-    src: url('${LucidaConsoleEot}');
-    src: url('${LucidaConsoleEot}#iefix') format('embedded-opentype'),
-         url('${LucidaConsoleWoff}') format('woff'),
-         url('${LucidaConsoleTtf}') format('truetype'),
-         url('${LucidaConsoleSvg}#LucidaConsole') format('svg');
+    src: url('${LucidaConsoleWoff}');
 }
 `
