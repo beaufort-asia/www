2ccf7aa87b8ea9bf1f8f64c48ad1153925c240d4
Sebastian Nemeth <sebastian@monkmage.com>
2020-04-21 10:55:44 +0300

    Add google analytics tracking tag

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -4,6 +4,7 @@ import { apiEndpoint } from '../prismic-configuration'
 import { PreviewSync } from './PreviewSync';
 import { PreviewLoading } from './PreviewLoading';
 
+declare var dataLayer: any;
 /**
  * Meta data component
  */
@@ -15,6 +16,15 @@ const Meta = () => {
   return (
     <Fragment>
       <Head>
+        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-27468128-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-27468128-1');
+        </script>
         <title>Beaufort</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
